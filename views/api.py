29a6592b5e253cc3404ec2ee6cb58325c945ce81
Mac-lp3 <staticlp3@gmail.com>
2016-09-17 14:22:11 -0400

    added exception json renderer and updated resource name

--- views/api.py
+++ views/api.py
@@ -1,11 +1,13 @@
+import json
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.view import view_config
 
 def put_date_range(request):
 	"""
-	Update an existing date rang in this hashLanding
+	Update an existing date rang in this eventLanding
 
-	If this hashLanding has existing date ranges, this will update
+	If this eventLanding has existing date ranges, this will update
 	the one with the correspodning composite id.
 	"""
 
@@ -13,9 +15,9 @@ def put_date_range(request):
 
 def delete_date_range(request):
 	"""
-	Delete an existing date rang in this hashLanding
+	Delete an existing date rang in this eventLanding
 
-	If this hashLanding has existing date ranges, this will delete
+	If this eventLanding has existing date ranges, this will delete
 	the one with the correspodning composite id.
 	"""
 
@@ -23,17 +25,27 @@ def delete_date_range(request):
 
 def post_date_range(request):
 	"""
-	Create a date rang in this hashLanding
+	Create a date rang in this eventLanding
 
 	This will create a new date range with a generated composite id
-	for this hashLanding.
+	for this eventLanding.
 	"""
+
+	try: 
+		postData = request.json_body
+		print(postData);
+	except json.decoder.JSONDecodeError:
+		raise HTTPBadRequest
+
+	postData = request.json_body
 
 	return Response("Ya dang post")
 
 def includeme(config):
-	config.add_route('hashLanding', '/api/{hashId}')
-	config.add_view(put_date_range, route_name='hashLanding', request_method="PUT")
-	config.add_view(post_date_range, route_name='hashLanding', request_method="POST")
-	config.add_view(delete_date_range, route_name='hashLanding', request_method="DELETE")
-	"""config.add_view(post_date_range, route_name='hashLanding')"""
+	config.add_route('eventLanding', '/api/event/{hashId}')
+	config.add_renderer('eventLanding', 'pyramid.renderers.json_renderer_factory')
+	config.add_view(put_date_range, route_name='eventLanding', request_method="PUT")
+	config.add_view(post_date_range, route_name='eventLanding', request_method="POST")
+	config.add_view(delete_date_range, route_name='eventLanding', request_method="DELETE")
+
+
