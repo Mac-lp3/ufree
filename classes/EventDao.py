ba222e6a4c5914b26db1ae39425b98ced3985acf
Mac-lp3 <staticlp3@gmail.com>
2016-10-11 19:52:43 -0400

    fixed class variable references

--- classes/EventDao.py
+++ classes/EventDao.py
@@ -1,41 +1,49 @@
 import psycopg2
 
-try:
-	conn = psycopg2.connect("dbname='ufree'")
-	cur = conn.cursor()
-except:
-	print("I am unable to connect to the database")
+class EventDao:
 
-def load_event(eventId):
+	try:
+		conn = psycopg2.connect("dbname='ufree'")
+		cur = conn.cursor()
+	except:
+		cur = {}
+		print("I am unable to connect to the database")
 
-	cur.execute("SELECT id, name from events WHERE id=" + str(eventId))
-	eventRows = cur.fetchall()
+	def load_event(eventId):
 
-	cur.execute("SELECT id, creator, fromdate, todate from dateRanges WHERE eventid=" + str(eventId))
-	dateRangeRows = cur.fetchall()
+		EventDao.cur.execute("SELECT id, name from events WHERE id=" + str(eventId))
+		eventRows = EventDao.cur.fetchall()
 
-	if (len(eventRows) == 1):
+		EventDao.cur.execute("SELECT id, creator, fromdate, todate from dateRanges WHERE eventid=" + str(eventId))
+		dateRangeRows = EventDao.cur.fetchall()
 
-		# Single record found.
-		# built the return object
-		dateRangeData = []
-		for dateRange in dateRangeRows:
-			dateRangeData.append({
-				'id': dateRange[0],
-				'creator': dateRange[1],
-				'fromDate': dateRange[2],
-				'toDate': dateRange[3]
-			})
+		if (len(eventRows) == 1):
 
-		data = {}
-		data['id'] = eventRows[0][0]
-		data['name'] = eventRows[0][1]
-		data['dateRanges'] = dateRangeData
-		return data
+			# Single record found.
+			# built the return object
+			dateRangeData = []
+			for dateRange in dateRangeRows:
+				dateRangeData.append({
+					'id': dateRange[0],
+					'creator': dateRange[1],
+					'fromDate': dateRange[2],
+					'toDate': dateRange[3]
+				})
 
-	elif (len(eventRows) > 1):
-		print('need to do a thing here')
+			data = {}
+			data['id'] = eventRows[0][0]
+			data['name'] = eventRows[0][1]
+			data['dateRanges'] = dateRangeData
+			return data
 
-	else:
-		# ID doesn't exist
-		# raise HTTPNotFound
+		elif (len(eventRows) > 1):
+			print('need to do a thing here')
+
+		else:
+			# ID doesn't exist
+			# raise HTTPNotFound
+			print('need to do a thing here')
+
+	def save_event(eventObject):
+
+		return EventDao.cur
