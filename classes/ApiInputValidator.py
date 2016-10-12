0afcbcd32c93efd7f5dafdf2feb8c70bca4466c8
Mac-lp3 <staticlp3@gmail.com>
2016-10-11 22:10:28 -0400

    class to encapsulate input validation

---
+++ classes/ApiInputValidator.py
@@ -0,0 +1,15 @@
+class ApiInputValidator:
+
+	def validateEvent(eventObject):
+		"""
+		Validates a given event object
+		"""
+
+		print('got to validateEvent')
+
+	def validateDateRange(dateRangeObject):
+		"""
+		Validates a given date range object
+		"""
+
+		print('got to validateDateRange')
