28681056822aebc5bb770835f32d81b49ca73196
Mac-lp3 <staticlp3@gmail.com>
2016-11-15 22:55:03 -0500

    util class to contain methods related to generating hash codes

---
+++ classes/HashCodeUtils.py
@@ -0,0 +1,14 @@
+class HashCodeUtils:
+
+	def generate_code(seed):
+	"""
+	Generates a new 64 bit FNV-1a hash with the provided seed.
+
+	"""
+	hval = 0x811c9dc5
+	fnv_32_prime = 0x01000193
+	uint32_max = 2 ** 32
+	for s in seed:
+		hval = hval ^ ord(s)
+		hval = (hval * fnv_32_prime) % uint32_max
+	return hval
