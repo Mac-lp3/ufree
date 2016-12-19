9c9c47747c410605adbc1c1c68f26e74b6a65f9b
Mac-lp3 <staticlp3@gmail.com>
2016-12-18 21:40:02 -0500

    fixed issue with variable names

--- classes/HashCodeUtils.py
+++ classes/HashCodeUtils.py
@@ -8,7 +8,8 @@ class HashCodeUtils:
 		hval = 0x811c9dc5
 		fnv_32_prime = 0x01000193
 		uint32_max = 2 ** 32
-		for s in str:
+		for s in seed:
 			hval = hval ^ ord(s)
 			hval = (hval * fnv_32_prime) % uint32_max
+
 		return hval
