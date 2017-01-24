b836be251b30c31c41b36fbddf6d2115f7d72020
Mac-lp3 <staticlp3@gmail.com>
2017-01-24 16:58:40 -0500

    removed static view for html partials. adding custom handler method

--- views/index.py
+++ views/index.py
@@ -8,10 +8,16 @@ def index(request):
     data = openFile.read()
     return Response(data, content_type='text/html')
 
+def partials(request):
+	partial = request.matchdict['partial']
+	print("subpath: " + request.subpath)
+
 def includeme(config):
     config.add_route('index', '/')
     config.add_view(index, route_name='index')
 
+    config.add_route('partials', '/partials/{partial}')
+    config.add_view(partials, route_name='partials')
+
     config.add_static_view(name='js', path='../static/js')
     config.add_static_view(name='css', path='../static/css')
-    config.add_static_view(name='app*', path='../app')
