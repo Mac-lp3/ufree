0efc285539f7633d24f68135b5393f0b6a8fc6cf
Mac-lp3 <staticlp3@gmail.com>
2017-04-11 17:20:23 -0400

    partials api now works properly

--- views/index.py
+++ views/index.py
@@ -1,16 +1,20 @@
 import os
 from pyramid.response import Response
 
+here = os.path.dirname(__file__)
+
 def index(request):
-    here = os.path.dirname(__file__)
     index_html = os.path.join(here, '../app/template.html')
     openFile = open(index_html)
     data = openFile.read()
     return Response(data, content_type='text/html')
 
 def partials(request):
-	partial = request.matchdict['partial']
-	print("subpath: " + request.subpath)
+    partial_file = request.matchdict['partial']
+    partial_html = os.path.join(here, '../app/partials/' + partial_file)
+    openFile = open(partial_html)
+    data = openFile.read()
+    return Response(data, content_type='text/html')
 
 def includeme(config):
     config.add_route('index', '/')
