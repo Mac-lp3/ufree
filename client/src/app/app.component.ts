c9cf8e7350a21308d0835374414fa62c90dd314f
Mac-lp3 <staticlp3@gmail.com>
2017-04-15 11:24:24 -0400

    now imports and uses navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `
+  <navbar></navbar>
+  <h1>Hello {{name}}</h1>
+  `
 })
 export class AppComponent { name = 'Angular'; }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarModule }  from '../decorators/navbar.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
