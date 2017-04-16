e012e9ffcc4066650bd862de9aa92dab021e7ad5
Mac-lp3 <staticlp3@gmail.com>
2017-04-16 17:10:25 -0400

    fixed import and wired event service to component

--- client/src/app/event/event.component.ts
+++ client/src/app/event/event.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { DateRange } from '../dateRange/dateRange.component'
+import { EventService } from '../services/event.service'
 
 @Component({
   selector: 'event',
   templateUrl: 'event.component.html'
 })
 export class EventComponent {
-
+	constuctor(eventService: EventService ) {
+		eventService.getEventById("1").subscribe((event) => {
+			console.log(event);
+		});
+	};
 }
 
 export interface Event {
