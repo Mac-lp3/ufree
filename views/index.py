12657a90fb7595ab394e69c58fa4914a300a4801
Mac-lp3 <staticlp3@gmail.com>
2017-04-16 13:00:23 -0400

    updated routes to work with angular2 file locations

--- views/index.py
+++ views/index.py
@@ -4,15 +4,14 @@ from pyramid.response import Response
 here = os.path.dirname(__file__)
 
 def index(request):
-    index_html = os.path.join(here, '../client/index.html')
+    index_html = os.path.join(here, '../client/src/index.html')
     openFile = open(index_html)
     data = openFile.read()
     return Response(data, content_type='text/html')
 
-def partials(request):
-    partial_file = request.matchdict['partial']
-    partial_html = os.path.join(here, '../client/partials/' + partial_file)
-    openFile = open(partial_html)
+def bundle(request):
+    bundle_html = os.path.join(here, '../client/dist/main.bundle.js')
+    openFile = open(bundle_html)
     data = openFile.read()
     return Response(data, content_type='text/html')
 
@@ -20,8 +19,5 @@ def includeme(config):
     config.add_route('index', '/')
     config.add_view(index, route_name='index')
 
-    config.add_route('partials', '/partials/{partial}')
-    config.add_view(partials, route_name='partials')
-
-    config.add_static_view(name='js', path='../static/js')
-    config.add_static_view(name='css', path='../static/css')
+    config.add_route('bundle', '/main.bundle.js')
+    config.add_view(bundle, route_name='bundle')
