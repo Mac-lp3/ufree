d24055e7962852e1159361875bc6cf04f0c51c32
Mac-lp3 <staticlp3@gmail.com>
2017-04-20 13:07:59 -0400

    added footer styles and wired it to component

--- client/src/app/decorators/footer.component.ts
+++ client/src/app/decorators/footer.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ufooter',
-  templateUrl: 'footer.component.html'
+  templateUrl: 'footer.component.html',
+  styleUrls:  ['./footer.component.css']
 })
 export class FooterComponent {
 
