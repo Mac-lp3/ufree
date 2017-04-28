d101b471f6519308cc6dfd8738c9e92375e5eebd
Mac-lp3 <staticlp3@gmail.com>
2017-04-28 15:58:40 -0400

    wired post event call to button

--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component }    from '@angular/core';
+import { Router }       from '@angular/router';
+import { EventService } from '../services/event.service'
 
 @Component({
   selector: 'landing',
   host: { class: 'Site-content' },
   templateUrl: 'landing.component.html',
-  styleUrls:  ['./landing.component.sass']
+  styleUrls:  ['./landing.component.sass'],
+  providers: [EventService]
 })
 export class LandingComponent {
 
+  constructor(private _eventService: EventService, private _router: Router) {
+
+  }
+
+  postEvent() {
+    let id = '101010101';
+    this._router.navigate(['events/' + id]);
+  }
 }
