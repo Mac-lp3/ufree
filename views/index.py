e5faff8cedb6a973341e51abd5f5daf1b5ece5f2
Mac-lp3 <staticlp3@gmail.com>
2017-04-30 20:36:23 -0400

    added methods to serve static files

--- views/index.py
+++ views/index.py
@@ -1,5 +1,5 @@
 import os
-from pyramid.response import Response
+from pyramid.response import Response, FileResponse
 
 here = os.path.dirname(__file__)
 
@@ -10,10 +10,24 @@ def index(request):
     return Response(data, content_type='text/html')
 
 def bundle(request):
-    bundle_html = os.path.join(here, '../client/dist/main.bundle.js')
-    openFile = open(bundle_html)
+    bundle_js = os.path.join(here, '../client/dist/main.bundle.js')
+    openFile = open(bundle_js)
     data = openFile.read()
     return Response(data, content_type='text/html')
+
+def styles(request):
+    styles_css = os.path.join(here, '../client/src/main.css')
+    openFile = open(styles_css)
+    data = openFile.read()
+    return Response(data, content_type='text/html')
+
+def landing_image(request):
+    asset = os.path.join(here, '../client/src/assets/color-tablet.jpeg')
+    return FileResponse(
+        asset,
+        request=request,
+        content_type='image/jpeg'
+    )
 
 def includeme(config):
     config.add_route('index', '/')
@@ -21,3 +35,9 @@ def includeme(config):
 
     config.add_route('bundle', '/main.bundle.js')
     config.add_view(bundle, route_name='bundle')
+
+    config.add_route('styles', '/main.css')
+    config.add_view(styles, route_name='styles')
+
+    config.add_route('landing_image', '/assets/color-tablet.jpeg')
+    config.add_view(landing_image, route_name='landing_image')
