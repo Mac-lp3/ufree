fa6f99b3fc0d77dc7bfeba90d3a3940e199f7d4f
Mac-lp3 <staticlp3@gmail.com>
2017-05-02 22:55:50 -0400

    now uses psycopg2 to create database if it does not exist

--- build.py
+++ build.py
@@ -1,23 +1,31 @@
 from pybuilder.core import use_plugin, task
+from psycopg2 import connect
 import subprocess
 import sys
 import os
+import json
 
-use_plugin("python.core")
+use_plugin('python.core')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+config = {}
+with open('build_config.json') as json_data_file:
+    config = json.load(json_data_file)
+
 def start_db ():
+    # get status of the /database directory
     db_status = subprocess.Popen(
         ['pg_ctl', 'status', '-D', './database'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
-
     out, err = db_status.communicate()
-    check_string = b'is not a database cluster'
 
+    # check output for 'is not database...' message
+    check_string = b'is not a database cluster'
     if check_string in out or check_string in err:
-        print('Database not found. Initializing...')
+        # initialize cluster if found
+        print('Database cluster not found. Initializing...')
         create_db = subprocess.Popen(
             ['pg_ctl', '-D', './database', 'initdb'],
             stdout=subprocess.PIPE
@@ -26,11 +34,46 @@ def start_db ():
         if create_db.returncode > 0:
             sys.exit()
 
-    print('Starting database...')
-    start_db = subprocess.Popen(
-        ['pg_ctl', 'start', '-D', './database', '-l', './db-logfile'],
-        stdin=subprocess.PIPE
+    # check if ufree database exists
+    arg_array = ['psql', '-U', config['user'], -'ufree', '-W', config['passwd']]
+    if not config['passwd']:
+        arg_array = ['psql', '-U', config['user'], 'ufree']
+
+    db_exist = subprocess.Popen(
+        arg_array,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
     )
+    out, err = db_status.communicate()
+
+    # check output for 'does not exist...' string
+    check_string = b'FATAL:  database \"ufree\" does not exist'
+    if check_string in out or check_string in err:
+        # initialize cluster if found
+        print('ufree database not found. creating...')
+        con = connect(
+            user = config['user'],
+            host = 'localhost',
+            password = config['passwd'],
+            port = str(config['port'])
+        )
+        cur = con.cursor()
+        cur.execute('CREATE DATABASE ufree')
+        cur.close()
+        con.close()
+
+    port_arg = '\"-p ' + str(config['postgres']['port']) + '\"'
+    print('Starting database...')
+    start_db = subprocess.Popen([
+        'pg_ctl',
+        '-o',
+        port_arg,
+        '-D',
+        './database',
+        '-l',
+        './db-logfile',
+        'start'
+    ], stdin=subprocess.PIPE)
     print(start_db.communicate())
     if start_db.returncode > 0:
         sys.exit()
