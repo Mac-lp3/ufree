11b9255aa409f1d697822ceda8eca7ebc508214e
Mac-lp3 <staticlp3@gmail.com>
2017-05-05 21:13:05 -0400

    uses single quotes consitantly

--- classes/HashCodeUtils.py
+++ classes/HashCodeUtils.py
@@ -1,9 +1,9 @@
 class HashCodeUtils:
 
 	def generate_code(seed):
-		"""
+		'''
 		Generates a new 64 bit FNV-1a hash with the provided seed.
-		"""
+		'''
 
 		hval = 0x811c9dc5
 		fnv_32_prime = 0x01000193
