6074a575fbd41d4554cfe6c83c2fff89b2a3329b
Mac-lp3 <staticlp3@gmail.com>
2017-05-05 21:13:27 -0400

    changed type of columns. specified length of month columns

--- ufree_dev.sql
+++ ufree_dev.sql
@@ -1,33 +1,33 @@
 CREATE TABLE availability (
     id integer SERIAL NOT NULL PRIMARY KEY,
     attendee_id integer NOT NULL,
-    event_id integer NOT NULL,
+    event_id varchar NOT NULL,
     year date,
-    january string,
-    february string,
-    march string,
-    april string,
-    may string,
-    june string,
-    july string,
-    august string,
-    september string,
-    october string,
-    november string,
-    december string
+    january varchar(31),
+    february varchar(29),
+    march varchar(31),
+    april varchar(30),
+    may varchar(31),
+    june varchar(30),
+    july varchar(31),
+    august varchar(31),
+    september varchar(30),
+    october varchar(31),
+    november varchar(30),
+    december varchar(31)
 );
 
 CREATE TABLE attendee (
   id integer SERIAL NOT NULL PRIMARY KEY,
-  name character NOT NULL,
-  event_id character NOT NULL,
+  name varchar(15) NOT NULL,
+  event_id varchar NOT NULL,
   joined_date date,
-  email character
+  email varchar(50)
 )
 
 CREATE TABLE events (
-    id character NOT NULL,
-    name character varying(30) NOT NULL,
+    id varchar NOT NULL,
+    name varchar(20) NOT NULL,
     creator_id integer NOT NULL,
     created_date date
 );
