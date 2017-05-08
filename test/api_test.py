c392a8fd3b661d8d03171c183be88356a3b31dca
Mac-lp3 <staticlp3@gmail.com>
2017-05-08 19:11:03 -0400

    added test for the api controller

---
+++ test/api_test.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+fts = os.path.join(dir_path, '..\\views\\api.py')
+exec(open(fts).read())
+
+class ApiTest(unittest.TestCase):
+
+    def get_event_test (self):
+        print(get_event)
+        resp = get_event({'eventId': '123123123123123'})
+        print('The resp', resp)
