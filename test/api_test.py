74a5238ad5f893a63042a7a213697de1fecd2f5c
Mac-lp3 <staticlp3@gmail.com>
2017-05-09 22:19:25 -0400

    added test cases for all forms of bad event ID

--- test/api_test.py
+++ test/api_test.py
@@ -7,10 +7,23 @@ exec(open(fts).read())
 
 class ApiTest(unittest.TestCase):
 
+    __short_id = '123123123123123'
+    __long_id = '123123123123123adsasdasdasdasj12jkas'
+    __bad_char_id = 'sdkjhdsfakjh3232ksjdn$sdlk1dsw12'
+
     def get_event_test (self):
-        print(get_event)
-        resp = get_event({'eventId': '123123123123123'})
-        print('The resp', resp)
+        # test bad id - too short
+        resp = get_event({'eventId': self.__short_id})
+        self.assertEqual(type(resp), HTTPBadRequest)
+
+        # test bad id - too long
+        resp = get_event({'eventId': self.__long_id})
+        self.assertEqual(type(resp), HTTPBadRequest)
+
+        # test bad id - too incorrect characters
+        resp = get_event({'eventId': self.__bad_char_id})
+        self.assertEqual(type(resp), HTTPBadRequest)
+
 
 if __name__ == '__main__':
 	unittest.main()
