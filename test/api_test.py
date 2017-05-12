80948f0019a691b31184364027b7b4d0a4678b0f
Mac-lp3 <staticlp3@gmail.com>
2017-05-11 22:21:54 -0400

    tests now work. added case for non-existing id

--- test/api_test.py
+++ test/api_test.py
@@ -1,4 +1,5 @@
 import os
+import json
 import unittest
 from views.api import get_event
 from pyramid import testing
@@ -8,31 +9,45 @@ class ApiTest(unittest.TestCase):
 
     __short_id = '123123123123123'
     __long_id = '123123123123123adsasdasdasdasj12jkas'
+    __non_exist_id = 'asd234fgh234asd123dfg123dfg23dfg'
     __bad_char_id = 'sdkjhdsfakjh3232ksjdn$sdlk1dsw12'
-    __good_id = 'asd234fgh234asd123dfg123dfg23dfg'
+    __good_id = 'qwe1fd23qwe123qwe123qwe123asd345'
 
     def get_event_fail_test (self):
         # test bad id - too short
         req = MockRequest(self.__short_id)
         resp = get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
+        self.assertTrue('Hash is less than 32 characters' in resp.json_body)
 
         # test bad id - too long
         req = MockRequest(self.__long_id)
         resp = get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
+        self.assertTrue('Hash is greater than 32 characters' in resp.json_body)
 
-        # test bad id - too incorrect characters
+        # test bad id - incorrect characters
         req = MockRequest(self.__bad_char_id)
         resp = get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
+        self.assertTrue('Hash can only include numbers and letters' in resp.json_body)
+
+        # test bad id - incorrect characters
+        req = MockRequest(self.__non_exist_id)
+        resp = get_event(req)
+        self.assertEqual(type(resp), HTTPBadRequest)
+        self.assertEqual(
+            'An error occurred while loading this event.',
+            resp.json_body['errors']
+        )
 
     def get_event_success_test (self):
         # test bad id - too short
         req = MockRequest(self.__good_id)
         resp = get_event(req)
-        print('the resp', resp)
-        self.assertEqual(1, HTTPBadRequest)
+        jbod = json.loads(resp)
+        self.assertNotEqual(resp, HTTPBadRequest)
+        self.assertEqual(self.__good_id, jbod['id'])
 
 class MockRequest():
 
