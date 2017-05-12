b9c515454311cee099157ec220485e5d892d36dc
Mac-lp3 <staticlp3@gmail.com>
2017-05-12 19:45:37 -0400

    now imports all of api, not just a function. MockRequest constructor now allows for json_body

--- test/api_test.py
+++ test/api_test.py
@@ -1,40 +1,49 @@
 import os
 import json
 import unittest
-from views.api import get_event
+import views.api as api
 from pyramid import testing
 from pyramid.httpexceptions import HTTPBadRequest
 
 class ApiTest(unittest.TestCase):
 
+    # test event ids
     __short_id = '123123123123123'
     __long_id = '123123123123123adsasdasdasdasj12jkas'
     __non_exist_id = 'asd234fgh234asd123dfg123dfg23dfg'
     __bad_char_id = 'sdkjhdsfakjh3232ksjdn$sdlk1dsw12'
     __good_id = 'qwe1fd23qwe123qwe123qwe123asd345'
 
+    # test event names
+    __numeric_name = 1337
+
+    def post_event_fail_test (self):
+        #test bad event name - not string
+        req = MockRequest(self.__short_id)
+
+
     def get_event_fail_test (self):
         # test bad id - too short
         req = MockRequest(self.__short_id)
-        resp = get_event(req)
+        resp = api.get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
         self.assertTrue('Hash is less than 32 characters' in resp.json_body)
 
         # test bad id - too long
         req = MockRequest(self.__long_id)
-        resp = get_event(req)
+        resp = api.get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
         self.assertTrue('Hash is greater than 32 characters' in resp.json_body)
 
         # test bad id - incorrect characters
         req = MockRequest(self.__bad_char_id)
-        resp = get_event(req)
+        resp = api.get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
         self.assertTrue('Hash can only include numbers and letters' in resp.json_body)
 
         # test bad id - incorrect characters
         req = MockRequest(self.__non_exist_id)
-        resp = get_event(req)
+        resp = api.get_event(req)
         self.assertEqual(type(resp), HTTPBadRequest)
         self.assertEqual(
             'An error occurred while loading this event.',
@@ -44,7 +53,7 @@ class ApiTest(unittest.TestCase):
     def get_event_success_test (self):
         # test bad id - too short
         req = MockRequest(self.__good_id)
-        resp = get_event(req)
+        resp = api.get_event(req)
         jbod = json.loads(resp)
         self.assertNotEqual(resp, HTTPBadRequest)
         self.assertEqual(self.__good_id, jbod['id'])
@@ -52,9 +61,12 @@ class ApiTest(unittest.TestCase):
 class MockRequest():
 
     matchdict = {}
+    json_body = {}
 
-    def __init__ (self, id):
+    def __init__ (self, id, body={}):
         self.matchdict = {'eventId': id}
+        if body:
+            json_body = body
 
 if __name__ == '__main__':
 	unittest.main()
