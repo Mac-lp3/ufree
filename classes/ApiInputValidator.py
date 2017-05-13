2659cae27559c9a7e4f875e0c362352e2b25b664
Mac-lp3 <staticlp3@gmail.com>
2017-05-13 10:30:23 -0400

    now checks if dictionary keys exist

--- classes/ApiInputValidator.py
+++ classes/ApiInputValidator.py
@@ -19,29 +19,35 @@ class ApiInputValidator:
 		error_messages = []
 
 		# Validate name field
-		if not isinstance(eventObject['name'], str):
-			error_messages.append('Name must be a string')
+		if 'name' in eventObject:
+			if not isinstance(eventObject['name'], str):
+				error_messages.append('Name must be a string')
 
-		if len(eventObject['name']) > ApiInputValidator.event_name_length:
-			message = 'Name must be less than {0} characters'.format(
-				ApiInputValidator.event_name_length
-			)
-			error_messages.append()
+			if len(eventObject['name']) > ApiInputValidator.event_name_length:
+				message = 'Name must be less than {0} characters'.format(
+					ApiInputValidator.event_name_length
+				)
+				error_messages.append()
 
-		if re.search(ApiInputValidator.event_name_pattern , eventObject['name']):
-			error_messages.append('Name must only contain letters or numbers')
+			if re.search(ApiInputValidator.event_name_pattern , eventObject['name']):
+				error_messages.append('Name must only contain letters or numbers')
+		else:
+			error_messages.append('Name is blank. A value for name is required')
 
 		# Validate creator field
-		if not isinstance(eventObject['creator'], str):
-			error_messages.append('Creator must be a string')
+		if 'creator' in eventObject:
+			if not isinstance(eventObject['creator'], str):
+				error_messages.append('Creator must be a string')
 
-		if len(eventObject['creator']) > ApiInputValidator.event_creator_length:
-			message = 'Creator must be less than {0} characters'.format(
-				ApiInputValidator.event_creator_length
-			)
-			error_messages.append(message)
+			if len(eventObject['creator']) > ApiInputValidator.event_creator_length:
+				message = 'Creator must be less than {0} characters'.format(
+					ApiInputValidator.event_creator_length
+				)
+				error_messages.append(message)
 
-		if re.search(ApiInputValidator.event_creator_pattern, eventObject['creator']):
-			error_messages.append('Creator must only contain letters or numbers')
+			if re.search(ApiInputValidator.event_creator_pattern, eventObject['creator']):
+				error_messages.append('Creator must only contain letters or numbers')
+		else:
+			error_messages.append('Creator is blank. A value for creator is required')
 
 		return error_messages
