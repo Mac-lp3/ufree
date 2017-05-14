502d93a7057b275e9b3707a03b52de4c28be9555
Mac-lp3 <staticlp3@gmail.com>
2017-05-14 12:22:49 -0400

    updated type checking for name input

--- classes/ApiInputValidator.py
+++ classes/ApiInputValidator.py
@@ -21,31 +21,39 @@ class ApiInputValidator:
 		# Validate name field
 		if 'name' in eventObject:
 			if not isinstance(eventObject['name'], str):
+				print('n instance check')
 				error_messages.append('Name must be a string')
 
-			if len(eventObject['name']) > self.__event_name_length:
+			elif len(eventObject['name']) > self.__event_name_length:
 				message = 'Name must be less than {0} characters'.format(
 					self.__event_name_length
 				)
-				error_messages.append()
+				error_messages.append(message)
 
-			if re.search(self.__event_name_pattern , eventObject['name']):
+			elif re.search(self.__event_name_pattern, eventObject['name']):
 				error_messages.append('Name must only contain letters or numbers')
+
+			else:
+				pass
 		else:
 			error_messages.append('Name is blank. A value for name is required')
 
 		# Validate creator field
 		if 'creator' in eventObject:
+			print('in c')
 			if not isinstance(eventObject['creator'], str):
+				print('in c instance')
 				error_messages.append('Creator must be a string')
 
 			if len(eventObject['creator']) > self.__event_creator_length:
+				print('in c len')
 				message = 'Creator must be less than {0} characters'.format(
 					self.__event_creator_length
 				)
 				error_messages.append(message)
 
 			if re.search(self.__event_creator_pattern, eventObject['creator']):
+				print('in c regex')
 				error_messages.append('Creator must only contain letters or numbers')
 		else:
 			error_messages.append('Creator is blank. A value for creator is required')
