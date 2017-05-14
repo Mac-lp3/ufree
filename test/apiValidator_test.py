4d5d2b20c04c0a659780b9a879b73c6702a16940
Mac-lp3 <staticlp3@gmail.com>
2017-05-14 13:07:27 -0400

    broke into two test cases

--- test/apiValidator_test.py
+++ test/apiValidator_test.py
@@ -1,56 +1,57 @@
 import os
 import unittest
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-fts = os.path.join(dir_path, '..\classes\ApiInputValidator.py')
-exec(open(fts).read())
+from classes.ApiInputValidator import ApiInputValidator
 
 class ApiInputValidatorTest(unittest.TestCase):
 
-    goodEvent = {
+    __goodEvent = {
         'id': 'kjhafu24kjsdncvhj23an3n32k1io213',
         'name': 'This is a good name',
         'creator': 'Mikey Big C'
     }
 
-    starNameEvent = {
+    __starNameEvent = {
         'id': 'kjhafu24kjsdncvhj23an3n32k1io213',
         'name': '* This is a bad name',
         'creator': 'Mikey Big C'
     }
 
-    closeNameEvent = {
+    __closeNameEvent = {
         'id': 'kjhafu24kjsdncvhj23an3n32k1io213',
         'name': ') This is a bad name',
         'creator': 'Mikey Big C'
     }
 
-    starCreatorEvent = {
+    __starCreatorEvent = {
         'id': 'kjhafu24kjsdncvhj23an3n32k1io213',
         'name': 'This is a good name',
         'creator': 'Mikey Big * C'
     }
 
-    closeCreatorEvent = {
+    __closeCreatorEvent = {
         'id': 'kjhafu24kjsdncvhj23an3n32k1io213',
         'name': 'This is a good name',
         'creator': ') Mikey Big C'
     }
 
-    def test_event_validation (self):
-        test_messages = ApiInputValidator.validate_event(self.starNameEvent)
+    __inputValidator = ApiInputValidator()
+
+    def test_event_fail_validation (self):
+        test_messages = self.__inputValidator.validate_event(self.__starNameEvent)
         self.assertEqual(len(test_messages), 1)
 
-        test_messages = ApiInputValidator.validate_event(self.closeNameEvent)
+        test_messages = self.__inputValidator.validate_event(self.__closeNameEvent)
         self.assertEqual(len(test_messages), 1)
 
-        test_messages = ApiInputValidator.validate_event(self.starCreatorEvent)
+        test_messages = self.__inputValidator.validate_event(self.__starCreatorEvent)
         self.assertEqual(len(test_messages), 1)
 
-        test_messages = ApiInputValidator.validate_event(self.closeCreatorEvent)
+        test_messages = self.__inputValidator.validate_event(self.__closeCreatorEvent)
         self.assertEqual(len(test_messages), 1)
 
-        test_messages = ApiInputValidator.validate_event(self.goodEvent)
+    def test_event_pass_validation (self):
+        test_messages = self.__inputValidator.validate_event(self.__goodEvent)
+        print(test_messages)
         self.assertEqual(len(test_messages), 0)
 
 if __name__ == '__main__':
