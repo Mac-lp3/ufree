44a48d0d4fa695e2d26bc89f27a23ee80514dc40
Mac-lp3 <staticlp3@gmail.com>
2017-05-16 20:45:31 -0400

    now throws exception based on buildins variables

--- test/classes/Cursor.py
+++ test/classes/Cursor.py
@@ -1,13 +1,25 @@
+import builtins
+
 class Cursor:
     def __init__(self):
-        print('initing')
+        print('Initializing Cursor')
 
     def execute (self, string):
+        if builtins.db_fail == 'True':
+            raise Exception('some exception')
         print('mocking ' + string)
 
     def fetchone (self):
+        if builtins.db_fail == 'True':
+            raise Exception('some exception')
         print('fetching one...')
-        return True
+        return {
+            'id': '123123123123',
+            'name': 'Kenny K\'s thing',
+            'creator': 'Kenny K'
+        }
 
     def fetchall (self):
+        if builtins.db_fail == 'True':
+            raise Exception('some exception')
         print('fetching all...')
