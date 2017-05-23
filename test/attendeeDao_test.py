f5e8f14426cab639836456dd1c2f93a53a56c918
Mac-lp3 <staticlp3@gmail.com>
2017-05-22 21:49:10 -0400

    initial tests for Attendee dao

---
+++ test/attendeeDao_test.py
@@ -0,0 +1,56 @@
+import os
+import unittest
+import builtins
+from classes.dao.AttendeeDao import AttendeeDao
+from classes.exception.DaoException import DaoException
+
+class AttendeeDaoTest(unittest.TestCase):
+
+	def setUp (self):
+		builtins.db_fail = os.environ['TEST_DB_FAIL']
+		self.__dao = AttendeeDao()
+
+	def load_attendee_test (self):
+		# test normal functionality
+		builtins.db_fail = 'False'
+		val = self.__dao.load_attendee('some id')
+		self.assertTrue('id' in val)
+		self.assertTrue('name' in val)
+		self.assertTrue('email' in val)
+
+		# test exception handeling
+		builtins.db_fail = 'True'
+		try:
+			val = self.__dao.load_attendee('some id')
+		except Exception as e:
+			self.assertTrue(isinstance(e, DaoException))
+
+	def save_attendee_test (self):
+		# test normal functionality
+		builtins.return_pattern = [
+			['abcd1234'],
+			['idk', 'some name', 'some@email.com'],
+			['abcd1234'],
+			['idk', 'some name', 'some@email.com'],
+		]
+		val = self.__dao.save_attendee({
+			'name': 'Some cool thing',
+			'email': 'idk@lol.gov'
+		})
+		self.assertTrue('id' in val)
+		self.assertTrue('name' in val)
+		self.assertTrue('email' in val)
+
+		# test unable to generate unique id
+		builtins.db_fail = 'True'
+		try:
+			self.__dao.save_attendee({
+				'name': 'Some cool thing',
+				'email': 'idk@lol.gov'
+			})
+		except Exception as e:
+			print(e)
+			self.assertTrue(isinstance(e, DaoException))
+
+	if __name__ == '__main__':
+		unittest.main()
