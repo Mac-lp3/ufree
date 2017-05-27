3d148d601382c28668a40c166752dfca06fcb8d8
Mac-lp3 <staticlp3@gmail.com>
2017-05-27 11:16:23 -0400

    initial delete method

--- classes/dao/AvailabilityDao.py
+++ classes/dao/AvailabilityDao.py
@@ -44,11 +44,43 @@ class AvailbilityDao:
         '''
         pass
 
-    def delete_availability (self):
+    def delete_availability (self, availability_id, attendee_id='', event_id=''):
         '''
         removes this users availability from the event.
         '''
-        pass
+        try:
+            if attendee_id:
+                if event_id:
+                    self.__cur.execute(
+                        'DELETE FROM availability WHERE attendee_id={0} '
+                        'AND event_id=\'{1}\''.format(
+                            attendee_id,
+                            event_id
+                        )
+                    )
+                else:
+                    self.__cur.execute(
+                        'DELETE FROM availability WHERE attendee_id={0}'.format(
+                            attendee_id
+                        )
+                    )
+            elif event_id
+                self.__cur.execute(
+                        'DELETE FROM availability WHERE event_id={0}'.format(
+                            event_id
+                        )
+                    )
+            else:
+                self.__cur.execute(
+                    'DELETE FROM availability WHERE id={0}'.format(
+                        availability_id
+                    )
+                )
+        except Exception as e:
+            print(e, sys.exc_info())
+            raise DaoException(
+                'Unknown error while deleting availability'
+            )
 
     def save_availability(self, availability):
         '''
@@ -64,21 +96,21 @@ class AvailbilityDao:
                 'november, december) VALUES ({0}, \'{1}\', \'{2}\', \'{3}\', '
                 '\'{4}\', \'{5}\', \'{6}\', \'{7}\', \'{8}\', \'{9}\', \'{10}\', '
                 '\'{11}\', \'{12}\', \'{13}\', \'{14}\')'.format(
-                availability['attendee_id'],
-                availability['event_id'],
-                availability['year'],
-                availability['january'],
-                availability['february'],
-                availability['march'],
-                availability['april'],
-                availability['may'],
-                availability['june'],
-                availability['july'],
-                availability['august'],
-                availability['september'],
-                availability['october'],
-                availability['november'],
-                availability['december']
+                    availability['attendee_id'],
+                    availability['event_id'],
+                    availability['year'],
+                    availability['january'],
+                    availability['february'],
+                    availability['march'],
+                    availability['april'],
+                    availability['may'],
+                    availability['june'],
+                    availability['july'],
+                    availability['august'],
+                    availability['september'],
+                    availability['october'],
+                    availability['november'],
+                    availability['december']
                 )
             )
 
