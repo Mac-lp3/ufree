4dccb5afa2e7797762e7814d84423bb05497ffce
Mac-lp3 <staticlp3@gmail.com>
2017-05-29 11:39:29 -0400

    added update test

--- test/availabilityDao_test.py
+++ test/availabilityDao_test.py
@@ -9,29 +9,63 @@ class AvailabilityDaoTest(unittest.TestCase):
     def setUp (self):
         builtins.db_fail = os.environ['TEST_DB_FAIL']
         self.__dao = AvailabilityDao()
+        self.availability_object = {
+            'id': 1234,
+            'attendee_id': 2345,
+            'event_id': 'lskdajfh234a231hlsadf234',
+            'year': '2017',
+            'january': '00000000000000000000000000',
+            'february': '00000000000000000000000000',
+            'march': '00000000000000000000000000',
+            'april': '00000000000000000000000000',
+            'may': '00000000000000000000000000',
+            'june': '00000000000000000000000000',
+            'july': '00000000000000000000000000',
+            'august': '00000000000000000000000000',
+            'september': '00000000000000000000000000',
+            'october': '00000000000000000000000000',
+            'november': '00000000000000000000000000',
+            'december': '00000000000000000000000000'
+        }
+        self.return_row = [
+            1234,
+            2345,
+            'lskdajfh234a231hlsadf234',
+            '2017',
+            'january',
+            'february',
+            'march',
+            'april',
+            'may',
+            'june',
+            'july',
+            'august',
+            'september',
+            'october',
+            'november',
+            'december'
+        ]
+
+    def update_availability_test (self):
+        builtins.return_pattern = [self.return_row]
+        obj = self.__dao.update_availability(self.availability_object)
+        self.assertTrue('id' in obj)
+        self.assertTrue('february' in obj)
+        self.assertTrue('august' in obj)
+        self.assertTrue('december' in obj)
+
+        # test exception handeling
+        builtins.db_fail = 'True'
+        try:
+            val = self.__dao.update_availability(self.availability_object)
+        except Exception as e:
+            self.assertTrue(isinstance(e, DaoException))
+        builtins.db_fail = 'False'
 
     def get_availability_test (self):
         # test attendee_id input
-        builtins.return_pattern = [
-            [
-                1234,
-                2345,
-                'lskdajfh234a231hlsadf234',
-                '2017',
-                'january',
-                'february',
-                'march',
-                'april',
-                'may',
-                'june',
-                'july',
-                'august',
-                'september',
-                'october',
-                'november',
-                'december'
-            ]
-        ]
+
+        builtins.return_pattern = [self.return_row]
         obj = self.__dao.get_availability(attendee_id=2345)
         self.assertTrue('id' in obj)
         self.assertTrue('february' in obj)
@@ -39,26 +73,7 @@ class AvailabilityDaoTest(unittest.TestCase):
         self.assertTrue('december' in obj)
 
         # test event_id input
-        builtins.return_pattern = [
-            [
-                1234,
-                2345,
-                'lskdajfh234a231hlsadf234',
-                '2017',
-                'january',
-                'february',
-                'march',
-                'april',
-                'may',
-                'june',
-                'july',
-                'august',
-                'september',
-                'october',
-                'november',
-                'december'
-            ]
-        ]
+        builtins.return_pattern = [self.return_row]
         obj = self.__dao.get_availability(event_id=1234)
         self.assertTrue('id' in obj)
         self.assertTrue('february' in obj)
@@ -66,26 +81,7 @@ class AvailabilityDaoTest(unittest.TestCase):
         self.assertTrue('december' in obj)
 
         # test event_id and attendee_id input
-        builtins.return_pattern = [
-            [
-                1234,
-                2345,
-                'lskdajfh234a231hlsadf234',
-                '2017',
-                'january',
-                'february',
-                'march',
-                'april',
-                'may',
-                'june',
-                'july',
-                'august',
-                'september',
-                'october',
-                'november',
-                'december'
-            ]
-        ]
+        builtins.return_pattern = [self.return_row]
         obj = self.__dao.get_availability(event_id=1234, attendee_id=2345)
         self.assertTrue('id' in obj)
         self.assertTrue('february' in obj)
