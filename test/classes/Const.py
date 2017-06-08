f01c1157db3173bfe956922527727903425eb1cd
Mac-lp3 <staticlp3@gmail.com>
2017-06-07 22:15:35 -0400

    Holds test constants

---
+++ test/classes/Const.py
@@ -0,0 +1,12 @@
+# use this user ID for everything going in / out of the DB
+GOOD_USER_ID = 'heyheyhey'
+
+# use this to test all cases where the user id does not exist
+BAD_USER_ID = 'watwatwat'
+
+# use these event IDs for everything going in / out of the DB
+GOOD_EVENT_ID = 'qwe1fd23qwe123qwe123qwe123asd345'
+MOCK_EVENT_IDS = [
+    GOOD_EVENT_ID,
+    '12345567890asdfghjklqwertyuiopzx'
+]
