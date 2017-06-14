0b4c99790f9cf8bd47679d41dd1f6f9de66a1b50
Mac-lp3 <staticlp3@gmail.com>
2017-06-14 16:15:52 -0400

    can now set attendee id in url param

--- test/classes/MockRequest.py
+++ test/classes/MockRequest.py
@@ -4,9 +4,11 @@ class MockRequest():
     json_body = {}
     cookies = {}
 
-    def __init__ (self, id={}, body={}, cookies={}):
+    def __init__ (self, id={}, body={}, cookies={}, attendee_id={}):
         if id:
-            self.matchdict = {'eventId': id}
+            self.matchdict['eventId'] = id
+        if attendee_id:
+            self.matchdict['attendee_id'] = attendee_id
         if body:
             self.json_body = body
         if cookies:
