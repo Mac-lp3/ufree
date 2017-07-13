48eaca614dffc8238d8eeab5cad11030dc54de11
Mac-lp3 <staticlp3@gmail.com>
2017-07-12 20:37:19 -0400

    initial commit

---
+++ test/attendeeValidator_test.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+import builtins
+from classes.util.AttendeeValidator import AttendeeValidator
+
+class AvailabilityDaoTest(unittest.TestCase):
+
+    def setUp (self):
+        self.__validator = AttendeeValidator()
+
+    def validate_attendee_request_test (self):
+        pass
+
+    def validate_attendee_name_test (self):
+        pass
+
+    def validate_attendee_id_test (self):
+        pass
