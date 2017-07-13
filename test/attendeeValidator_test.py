10e104f8ba14597f2af3c7f3e4c4596466a9df64
Mac-lp3 <staticlp3@gmail.com>
2017-07-13 08:09:16 -0400

    v1 of some tests

--- test/attendeeValidator_test.py
+++ test/attendeeValidator_test.py
@@ -2,6 +2,8 @@ import os
 import unittest
 import builtins
 from classes.util.AttendeeValidator import AttendeeValidator
+from classes.exception.ValidationException import ValidationException
+
 
 class AvailabilityDaoTest(unittest.TestCase):
 
@@ -12,7 +14,63 @@ class AvailabilityDaoTest(unittest.TestCase):
         pass
 
     def validate_attendee_name_test (self):
-        pass
+        # test type - bad
+        try:
+            self.__validator.validate_attendee_name(2345345)
+            self.assertTrue(False)
+        except Exception as e:
+            self.assertTrue(isinstance(e, ValidationException))
+
+        # test type - good
+        try:
+            self.__validator.validate_attendee_name('robby ray')
+        except Exception as e:
+            print(e)
+            self.assertTrue(False)
+
+        # test length - bad
+        try:
+            self.__validator.validate_attendee_name(
+                'asdfasdfasdfsdfasdfas12341243AB'
+            )
+            self.assertTrue(False)
+        except Exception as e:
+            self.assertTrue(isinstance(e, ValidationException))
+
+        # test length - good
+        try:
+            self.__validator.validate_attendee_name(
+                'asdfasdfasdfsdfasdfas12341243A'
+            )
+        except Exception as e:
+            print(e)
+            self.assertTrue(False)
+
+        # test format - bad
+        try:
+            self.__validator.validate_attendee_name(') DELETE *')
+            self.assertTrue(False)
+        except Exception as e:
+            self.assertTrue(isinstance(e, ValidationException))
+
+        # test format - good
+        try:
+            self.__validator.validate_attendee_name('DELETE')
+        except Exception as e:
+            print(e)
+            self.assertTrue(False)
 
     def validate_attendee_id_test (self):
-        pass
+        # test id - bad
+        try:
+            self.__validator.validate_attendee_name(') DELETE *')
+            self.assertTrue(False)
+        except Exception as e:
+            self.assertTrue(isinstance(e, ValidationException))
+
+        # test format - good
+        try:
+            self.__validator.validate_attendee_name('DELETE')
+        except Exception as e:
+            print(e)
+            self.assertTrue(False)
