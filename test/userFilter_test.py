1c509e87fb1cdbf4400b142d9c630bb9ae8f98a1
Mac-lp3 <staticlp3@gmail.com>
2017-07-19 22:33:47 -0400

    added v1 of test

--- test/userFilter_test.py
+++ test/userFilter_test.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import hashlib
+from test.classes.MockRequest import MockRequest
 from classes.filter.UserFilter import UserFilter
 
 class UserFilterTest(unittest.TestCase):
@@ -12,4 +13,11 @@ class UserFilterTest(unittest.TestCase):
         # test no cookies w/ name in body
         # test no cookies w/ creator in body
         # test no cookies w/ neither in body
-        pass
+        try:
+            req = MockRequest(body={'name': 'Jimmy'})
+            res = self.__filter.set_user_id(req)
+            print(res)
+        except Exception as e:
+            print(e)
+        print(UserFilter())
+        self.assertTrue(False)
