adf71b6ad0a6ec1deec7c8546f6ae4d39dd48640
Mac-lp3 <staticlp3@gmail.com>
2017-07-22 17:56:18 -0400

    added remaining tests

--- test/userFilter_test.py
+++ test/userFilter_test.py
@@ -3,6 +3,7 @@ import unittest
 import hashlib
 from test.classes.MockRequest import MockRequest
 from classes.filter.UserFilter import UserFilter
+from classes.exception.ValidationException import ValidationException
 
 class UserFilterTest(unittest.TestCase):
 
@@ -21,4 +22,20 @@ class UserFilterTest(unittest.TestCase):
             self.assertTrue(False)
 
         # test no cookies w/ creator in body
+        try:
+            req = MockRequest(body={'creator': 'Jimmy'})
+            res = self.__filter.set_user_id(req)
+            self.assertTrue(hasattr(res, 'cookies'))
+            self.assertTrue(res.cookies['user_id'])
+        except Exception as e:
+            print('exception', str(e))
+            self.assertTrue(False)
+
         # test no cookies w/ neither in body
+        try:
+            req = MockRequest()
+            res = self.__filter.set_user_id(req)
+            self.assertTrue(False, 'Should have thrown an exception')
+        except Exception as e:
+            print('exception', str(e))
+            self.assertTrue(isinstance(e, ValidationException))
