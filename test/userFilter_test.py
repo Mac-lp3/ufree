ce83f17cf78e1c8273ad567709e9dc2a65449645
Mac-lp3 <staticlp3@gmail.com>
2017-07-21 20:07:08 -0400

    first test functional

--- test/userFilter_test.py
+++ test/userFilter_test.py
@@ -11,13 +11,14 @@ class UserFilterTest(unittest.TestCase):
 
     def set_user_id_test (self):
         # test no cookies w/ name in body
-        # test no cookies w/ creator in body
-        # test no cookies w/ neither in body
         try:
             req = MockRequest(body={'name': 'Jimmy'})
             res = self.__filter.set_user_id(req)
-            print('response', res)
+            self.assertTrue(hasattr(res, 'cookies'))
+            self.assertTrue(res.cookies['user_id'])
         except Exception as e:
-            print('exception', e)
-        print(UserFilter())
-        self.assertTrue(False)
+            print('exception', str(e))
+            self.assertTrue(False)
+
+        # test no cookies w/ creator in body
+        # test no cookies w/ neither in body
