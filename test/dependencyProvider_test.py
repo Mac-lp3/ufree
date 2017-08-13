6f366700d348635276a8405e545bb2ad810fa3a5
Mac-lp3 <staticlp3@gmail.com>
2017-08-12 20:48:16 -0400

    initial test

---
+++ test/dependencyProvider_test.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import builtins
+from classes.provider.DependencyProvider import DependencyProvider
+
+class DependencyProviderTest(unittest.TestCase):
+
+    def setUp (self):
+        self._provider = DependencyProvider()
+
+    def get_instance_test (self):
+        # test normal behavior - test
+        dao = self._provider.get_instance('EventDao')
+        self.assertEqual(
+            dao.__module__ + "." + dao.__class__.__name__,
+            'test.classes.EventDao.type'
+        )
+
+        _filter = self._provider.get_instance('UserFilter')
+        self.assertEqual(
+            _filter.__module__ + "." + _filter.__class__.__name__,
+            'test.classes.UserFilter.type'
+        )
+
+        print(filter)
