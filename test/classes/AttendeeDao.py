f8d26a22431df622da51cce7f8c1f1ba235e8d56
Mac-lp3 <staticlp3@gmail.com>
2017-08-16 19:45:20 -0400

    tabs to spaces

--- test/classes/AttendeeDao.py
+++ test/classes/AttendeeDao.py
@@ -5,41 +5,41 @@ from classes.exception.DaoException import DaoException
 
 class AttendeeDao:
 
-	def leave_event (self, attendee_id, event_id):
-		print('leaving this event...')
+    def leave_event (self, attendee_id, event_id):
+        print('leaving this event...')
 
-	def save_attendee (self, attendee):
-		data = {
-			'id': 'asd',
-			'name': 'idkidkidk',
-			'email': 'idk@lol.com'
-		}
-		return data
+    def save_attendee (self, attendee):
+        data = {
+            'id': 'asd',
+            'name': 'idkidkidk',
+            'email': 'idk@lol.com'
+        }
+        return data
 
-	def update_attendee (self, attendee):
-		data = {
-			'id': 'asd',
-			'name': 'idkidkidk',
-			'email': 'idk@lol.com'
-		}
-		return data
+    def update_attendee (self, attendee):
+        data = {
+            'id': 'asd',
+            'name': 'idkidkidk',
+            'email': 'idk@lol.com'
+        }
+        return data
 
-	def load_attendee (self, attendee_id):
-		data = {
-			'id': 'asd',
-			'name': 'idkidkidk',
-			'email': 'idk@lol.com'
-		}
-		return data
+    def load_attendee (self, attendee_id):
+        data = {
+            'id': 'asd',
+            'name': 'idkidkidk',
+            'email': 'idk@lol.com'
+        }
+        return data
 
-	def load_event_attendees (self, event_id):
-		pass
+    def load_event_attendees (self, event_id):
+        pass
 
-	def attendee_exists (self, attendee_id):
-		return False
+    def attendee_exists (self, attendee_id):
+        return False
 
-	def delete_attendee (self, attendee):
-		pass
+    def delete_attendee (self, attendee):
+        pass
 
-	def join_event (self, attendee, event_id):
-		pass
+    def join_event (self, attendee, event_id):
+        pass
