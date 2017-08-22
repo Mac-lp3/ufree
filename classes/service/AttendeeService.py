c0648ad60e246ca01f8ef32d0282ba02c58a22a5
Mac-lp3 <staticlp3@gmail.com>
2017-08-21 21:04:28 -0400

    moved over methods from event service that return attendee objects

--- classes/service/AttendeeService.py
+++ classes/service/AttendeeService.py
@@ -27,6 +27,75 @@ class AttendeeService:
     def update_attendee (self, request):
         pass
 
+    def update_event_attendee (self, req):
+        # TODO this mehtod may not be needed. could just roll into update_attendee
+        response_body = {}
+        try:
+            if 'id' not in req.json_body:
+                raise ServiceException('Id was not found on this request')
+            inputErrors = self.__attendeeValidator.validate_attendee_request(req)
+            data = self.__attendee_dao.update_attendee(req.json_body)
+            response = Response(content_type='application/json')
+            response.charset = 'UTF-8'
+            response.json_body = json.dumps(data)
+        except BaseAppException as e:
+            # Handle DAO/Validation errors
+            raise ServiceException(str(e))
+        except Exception as e:
+            # Handle unexpected errors
+            print(e, sys.exc_info())
+            raise ServiceException(
+                'An error occurred while updating your info.'
+            )
+        return response
+
+    def update_attendee_availability (self, req):
+        req_body = req.json_body
+        response_body = {}
+        try:
+            # TODO validate availability
+            #inputErrors = self.__eventValidator.validate_attendee(req_body)
+            data = self.__availability_dao.update_availability(req_body)
+            response = Response(content_type='application/json', status=200)
+            response.charset = 'UTF-8'
+            response.json_body = json.dumps(data)
+        except BaseAppException as e:
+            # Handle DAO/Validation errors
+            raise ServiceException(str(e))
+        except Exception as e:
+            # Handle unexpected errors
+            print(e, sys.exc_info())
+            raise ServiceException(
+                'An error occurred while updating availability.'
+            )
+        return response
+
+    def get_event_attendees (self, req):
+        try:
+            # validate the event id
+            eventId = req.matchdict['eventId']
+            self.__eventValidator.validate_event_id(eventId)
+
+            # load the attendee list
+            data = self.__attendee_dao.load_event_attendees(eventId)
+
+            # build response object
+            response = Response(content_type='application/json', status=200)
+            response.charset = 'UTF-8'
+            response.json_body = json.dumps(data)
+
+        except BaseAppException as e:
+            # Handle DAO/Validation errors
+            raise ServiceException(str(e))
+        except Exception as e:
+            # Handle unexpected errors
+            print(e, sys.exc_info())
+            raise ServiceException(
+                'An error occurred while retrieving attendee list.'
+            )
+
+        return response
+
     def create_attendee (self, request):
         pass
 
