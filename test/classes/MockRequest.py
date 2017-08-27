9a7d327d738432a7520e15cb90e376ec15aa7cb4
Mac-lp3 <staticlp3@gmail.com>
2017-08-27 18:04:06 -0400

    renamed one of the contructor args

--- test/classes/MockRequest.py
+++ test/classes/MockRequest.py
@@ -4,9 +4,9 @@ class MockRequest():
     json_body = {}
     cookies = {}
 
-    def __init__ (self, id={}, body={}, cookies={}, attendee_id={}):
+    def __init__ (self, event_id={}, body={}, cookies={}, attendee_id={}):
         if id:
-            self.matchdict['eventId'] = id
+            self.matchdict['eventId'] = event_id
         if attendee_id:
             self.matchdict['attendee_id'] = attendee_id
         if body:
