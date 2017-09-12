58889e18f4b72fa3627b13390634d685914271ed
Mac-lp3 <staticlp3@gmail.com>
2017-09-11 21:55:28 -0400

    v1 of get even availability method

--- classes/service/AvailabilityService.py
+++ classes/service/AvailabilityService.py
@@ -22,7 +22,25 @@ class AvailabilityService:
         self.__eventValidator = EventValidator()
         self.__attendeeValidator = AttendeeValidator()
 
-        # get all availability obs for this event
+    # get all availability obs for this event
+    def get_event_availability (self, req):
+        try:
+            event_id = req.matchdict['eventId']
+            self.__eventValidator.validate_event_id(event_id)
+
+            data = self.__availability_dao.get_event_availability(event_id)
+            response = Response(content_type='application/json', status=200)
+            response.charset = 'UTF-8'
+            response.json_body = json.dumps(data)
+        except BaseAppException as e:
+            raise ServiceException(str(e))
+        except Exception as e:
+            print(e, sys.exc_info())
+            raise ServiceException(
+                'An error occurred while loading availability for event:',
+                event_id
+            )
+        return response
 
         # delete all availability obs for this event
 
