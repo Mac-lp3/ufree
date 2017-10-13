86dd662b0655a5e01b0b73eb64019ae3b199d273
Mac-lp3 <staticlp3@gmail.com>
2017-10-12 21:03:55 -0400

    added param for availability id. matchdict now intializes

--- test/classes/MockRequest.py
+++ test/classes/MockRequest.py
@@ -1,15 +1,29 @@
 class MockRequest():
 
-    matchdict = {}
+    matchdict = {
+        'eventId': None,
+        'attendee_id': None,
+        'availability_id': None
+    }
     json_body = {}
     cookies = {}
     method = 'GET'
 
-    def __init__ (self, event_id={}, body={}, cookies={}, attendee_id={}, method='GET'):
-        if id:
+    def __init__ (
+        self,
+        event_id={},
+        body={},
+        cookies={},
+        attendee_id={},
+        availability_id={},
+        method='GET'
+    ):
+        if event_id:
             self.matchdict['eventId'] = event_id
         if attendee_id:
             self.matchdict['attendee_id'] = attendee_id
+        if availability_id:
+            self.matchdict['availability_id'] = availability_id
         if body:
             self.json_body = body
         if cookies:
