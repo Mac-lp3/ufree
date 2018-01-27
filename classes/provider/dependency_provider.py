bec64b24dd9e34fca0d8e9f6640eac8d97e1ebab
Mac-LP3 <michael.calvo.a@gmail.com>
2018-01-27 10:01:47 -0500

    lint fixes and now pulls package names from OS.ENVIRON rather than builtins

--- classes/provider/dependency_provider.py
+++ classes/provider/dependency_provider.py
@@ -1,38 +1,38 @@
 import os
 import re
-import builtins
 import importlib
 from classes.exception.provider_exception import ProviderException
 
 class DependencyProvider:
 
-    def get_instance (self, class_name):
+    def get_instance(self, class_name):
         target = None
         package = ''
         target_class = None
         try:
             if 'dao' in class_name.lower():
-                package = builtins.daos_package
+                package = os.environ['DAOS_PACKAGE']
             elif 'filter' in class_name.lower():
-                package = builtins.filters_package
+                package = os.environ['FILTERS_PACKAGE']
             elif 'service' in class_name.lower():
-                package = builtins.services_package
+                package = os.environ['SERVICES_PACKAGE']
             elif 'psycopg2' in class_name.lower():
-                package = builtins.psycopg2_package
+                package = os.environ['PSYCOPG2_PACKAGE']
             target_class = importlib.import_module(
                 package + self.to_snake_case(class_name)
             )
             target = getattr(target_class, class_name)
-        except Exception as e:
+        except Exception as exception:
+            print('exception in dependency provider:', exception)
+            print('env vars:', os.environ)
             raise ProviderException(
                 'Could not locate dependency ' + class_name + ' in ' + package
             )
 
         target = getattr(target_class, class_name)
         target = target()
-        
         return target
 
     def to_snake_case(self, name):
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        sub_str = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', sub_str).lower()
