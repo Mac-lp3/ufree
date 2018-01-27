76048a2ccac1e5d00a331869d0e4c4531c00a571
Mac-LP3 <michael.calvo.a@gmail.com>
2018-01-27 10:01:04 -0500

    lint fixes

--- classes/exception/base_app_exception.py
+++ classes/exception/base_app_exception.py
@@ -2,11 +2,11 @@ import json
 
 class BaseAppException(Exception):
 
-    def __init__ (self, messages):
+    def __init__(self, messages):
         super()
         self.messages = messages
 
-    def get_payload (self):
+    def get_payload(self):
         return json.dumps({
             'errors': self.messages
         })
